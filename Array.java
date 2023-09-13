a25d29a9f2728c7b5edbeeda55f61b7ead06a1b8
Ali Mohamed <85517447+alimkhodr@users.noreply.github.com>
2023-09-13 13:56:06 -0300

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int qtd;
+        System.out.print("Digite a quantidade de numeros: ");
+        qtd = scanner.nextInt();
+
+        int numeros[] = new int[qtd];
+        int i = 0;
+        while (i < qtd) {
+            System.out.print("Digite um numero: ");
+            numeros[i] = scanner.nextInt();
+            i++;
+        }
+
+        int soma = 0;
+        for (int n : numeros) {
+            soma += n;
+        }
+
+        int media;
+        media = soma / 2;
+        System.out.println("A média é: "+media);
+
+        int maior = numeros[0];
+        for (int num : numeros) {
+            if (num > maior) {
+                maior = num;
+            }
+        }
+        System.out.println("O maior número é: " + maior);
+
+
+        scanner.close();
+    }
+}

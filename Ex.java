6f3ea889e9020bfd4fe8dd332c93ed915c7050ed
Ali Mohamed <85517447+alimkhodr@users.noreply.github.com>
2023-09-13 22:51:17 -0300

    Update Ex.java

--- Ex.java
+++ Ex.java
@@ -5,9 +5,11 @@ public class Ex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Escolha um exercício para executar:");
+            System.out.println("\nEscolha um exercício para executar:");
             System.out.println("1. Ex71");
             System.out.println("2. Ex72");
+            System.out.println("3. Vetor");
+            System.out.println("4. Ex78");
             System.out.println("0. Sair");
             System.out.print("-> ");
 
@@ -18,6 +20,12 @@ public class Ex {
                     break;
                 case 2:
                     Ex72(scanner);
+                    break;
+                case 3:
+                    Ex77(scanner);
+                    break;
+                case 4:
+                    Ex78(scanner);
                     break;
                 case 0:
                     System.out.println("Saindo do programa.");
@@ -98,4 +106,67 @@ public class Ex {
             scanner.next();
         }
     }
+
+    public static void Ex77(Scanner scanner) {
+        int n = 10; // tamanho do vetor
+        int v[] = new int[n]; // declaração do vetor "v"
+        int i; // índice ou posição
+
+        // Entrada de Dados
+        for (i = 0; i < n; i++) {
+            System.out.printf("Informe º%d. valor de %d: ", (i + 1), n);
+            v[i] = scanner.nextInt();
+        }
+
+        // Processamento: somar todos os valores, definir o maior e o menor valor
+        int soma = 0;
+        int menor = v[0]; // v[0] = 1o. valor armazenador no vetor "v"
+        int maior = v[0];
+        for (i = 0; i < n; i++) {
+            soma = soma + v[i];
+
+            if (v[i] < menor)
+                menor = v[i];
+
+            if (v[i] > maior)
+                maior = v[i];
+        }
+
+        // Saída (resultados)
+        System.out.printf("\n");
+        for (i = 0; i < n; i++) {
+            if (v[i] == menor)
+                System.out.printf("v[%d] = %d <--- menor valor\n", i, v[i]);
+            else if (v[i] == maior)
+                System.out.printf("v[%d] = %d <--- maior valor\n", i, v[i]);
+            else
+                System.out.printf("v[%d] = %d\n", i, v[i]);
+        }
+
+        System.out.printf("Soma = %d\n", soma);
+    }
+
+public static void Ex78(Scanner scanner) {
+    String nome[] = new String[10];
+    int i;
+    for (i = 0; i < 10; i++) {
+        System.out.printf("Informe o %dº nome de 10:", (i+1));
+        nome[i] = scanner.nextLine();
+    }
+    String proc;
+    System.out.printf("Procure um nome:");
+    proc = scanner.nextLine();
+    boolean encontrado = false; // Variável para verificar se o nome foi encontrado
+    for (i = 0; i < 10; i++) {
+        if (nome[i].equals(proc)) {
+            encontrado = true;
+            break; // Se o nome for encontrado, não é necessário continuar procurando
+        }
+    }
+    if (encontrado) {
+        System.out.println("Achei");
+    } else {
+        System.out.println("Não achei");
+    }
+}
 }
